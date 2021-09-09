3da1252947b1261cd1255668cb2d4139afded215
Suryansh Srivastava <suryanshsriv1@gmail.com>
2021-09-09 23:01:38 +0530

    added show page

--- src/App.js
+++ src/App.js
@@ -2,24 +2,38 @@ import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 
 import Home from './pages/Home';
+import Show from './pages/Show';
 import Starred from './pages/Starred';
 
 function App() {
-  return (
-    <Switch>
-      <Route exact path="/">
-        <Home />
-      </Route>
+    return ( <
+        Switch >
+        <
+        Route exact path = "/" >
+        <
+        Home / >
+        <
+        /Route>
 
-      <Route exact path="/starred">
-        <Starred />
-      </Route>
+        <
+        Route exact path = "/starred" >
+        <
+        Starred / >
+        <
+        /Route> <
+        Route exact path = "/show/:id" >
+        <
+        Show / >
+        <
+        /Route>
 
-      <Route>
-        <div>Not found</div>
-      </Route>
-    </Switch>
-  );
+        <
+        Route >
+        <
+        div > Not found < /div> <
+        /Route> <
+        /Switch>
+    );
 }
 
 export default App;
