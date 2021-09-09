3f7a72199542c07f87163e9d87911d436947000a
Suryansh Srivastava <suryanshsriv1@gmail.com>
2021-09-09 21:55:19 +0530

    crated preview cards for api data

--- src/components/show/ShowCard.js
+++ src/components/show/ShowCard.js
@@ -2,34 +2,26 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 const ShowCard = ({ id, image, name, summary }) => {
-    const summaryAsText = summary ?
-        `${summary.split(' ').slice(0, 10).join(' ').replace(/<.+?>/g, '')}...` :
-        'No description';
+  const summaryAsText = summary
+    ? `${summary.split(' ').slice(0, 10).join(' ').replace(/<.+?>/g, '')}...`
+    : 'No description';
 
-    return ( <
-        div >
-        <
-        div >
-        <
-        img src = { image }
-        alt = "show" / >
-        <
-        /div>
+  return (
+    <div>
+      <div>
+        <img src={image} alt="show" />
+      </div>
 
-        <
-        h1 > { name } < /h1>
+      <h1>{name}</h1>
 
-        <
-        p > { summaryAsText } < /p>
+      <p>{summaryAsText}</p>
 
-        <
-        div >
-        <
-        Link to = { `/show/${id}` } > Read more < /Link> <
-        button type = "button" > Star me < /button> <
-        /div> <
-        /div>
-    );
+      <div>
+        <Link to={`/show/${id}`}>Read more</Link>
+        <button type="button">Star me</button>
+      </div>
+    </div>
+  );
 };
 
 export default ShowCard;
