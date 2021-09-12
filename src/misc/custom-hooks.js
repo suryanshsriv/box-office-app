c41c7000f2c30afdcbe175e6faa3f4009d31668c
Suryansh Srivastava <suryanshsriv1@gmail.com>
2021-09-12 20:15:36 +0530

    commit on 'master'

--- src/misc/custom-hooks.js
+++ src/misc/custom-hooks.js
@@ -2,94 +2,98 @@ import { useReducer, useEffect, useState } from 'react';
 import { apiGet } from './config';
 
 function showsReducer(prevState, action) {
-  switch (action.type) {
-    case 'ADD': {
-      return [...prevState, action.showId];
-    }
+    switch (action.type) {
+        case 'ADD':
+            {
+                return [...prevState, action.showId];
+            }
 
-    case 'REMOVE': {
-      return prevState.filter(showId => showId !== action.showId);
-    }
+        case 'REMOVE':
+            {
+                return prevState.filter(showId => showId !== action.showId);
+            }
 
-    default:
-      return prevState;
-  }
+        default:
+            return prevState;
+    }
 }
 
 function usePersistedReducer(reducer, initialState, key) {
-  const [state, dispatch] = useReducer(reducer, initialState, initial => {
-    const persisted = localStorage.getItem(key);
+    const [state, dispatch] = useReducer(reducer, initialState, initial => {
+        const persisted = localStorage.getItem(key);
 
-    return persisted ? JSON.parse(persisted) : initial;
-  });
+        return persisted ? JSON.parse(persisted) : initial;
+    });
 
-  useEffect(() => {
-    localStorage.setItem(key, JSON.stringify(state));
-  }, [state, key]);
+    useEffect(() => {
+        localStorage.setItem(key, JSON.stringify(state));
+    }, [state, key]);
 
-  return [state, dispatch];
+    return [state, dispatch];
 }
 
 export function useShows(key = 'shows') {
-  return usePersistedReducer(showsReducer, [], key);
+    return usePersistedReducer(showsReducer, [], key);
 }
 
 export function useLastQuery(key = 'lastQuery') {
-  const [input, setInput] = useState(() => {
-    const persisted = sessionStorage.getItem(key);
+    const [input, setInput] = useState(() => {
+        const persisted = sessionStorage.getItem(key);
 
-    return persisted ? JSON.parse(persisted) : '';
-  });
+        return persisted ? JSON.parse(persisted) : '';
+    });
 
-  const setPersistedInput = newState => {
-    setInput(newState);
-    sessionStorage.setItem(key, JSON.stringify(newState));
-  };
+    const setPersistedInput = newState => {
+        setInput(newState);
+        sessionStorage.setItem(key, JSON.stringify(newState));
+    };
 
-  return [input, setPersistedInput];
+    return [input, setPersistedInput];
 }
 
 const reducer = (prevState, action) => {
-  switch (action.type) {
-    case 'FETCH_SUCCESS': {
-      return { isLoading: false, error: null, show: action.show };
-    }
+    switch (action.type) {
+        case 'FETCH_SUCCESS':
+            {
+                return { isLoading: false, error: null, show: action.show };
+            }
 
-    case 'FETCH_FAILED': {
-      return { ...prevState, isLoading: false, error: action.error };
-    }
+        case 'FETCH_FAILED':
+            {
+                return {...prevState, isLoading: false, error: action.error };
+            }
 
-    default:
-      return prevState;
-  }
+        default:
+            return prevState;
+    }
 };
 
 export function useShow(showId) {
-  const [state, dispatch] = useReducer(reducer, {
-    show: null,
-    isLoading: true,
-    error: null,
-  });
+    const [state, dispatch] = useReducer(reducer, {
+        show: null,
+        isLoading: true,
+        error: null,
+    });
 
-  useEffect(() => {
-    let isMounted = true;
+    useEffect(() => {
+        let isMounted = true;
 
-    apiGet(`/shows/${showId}?embed[]=seasons&embed[]=cast`)
-      .then(results => {
-        if (isMounted) {
-          dispatch({ type: 'FETCH_SUCCESS', show: results });
-        }
-      })
-      .catch(err => {
-        if (isMounted) {
-          dispatch({ type: 'FETCH_FAILED', error: err.message });
-        }
-      });
+        apiGet(`/shows/${showId}?embed[]=seasons&embed[]=cast`)
+            .then(results => {
+                if (isMounted) {
+                    dispatch({ type: 'FETCH_SUCCESS', show: results });
+                }
+            })
+            .catch(err => {
+                if (isMounted) {
+                    dispatch({ type: 'FETCH_FAILED', error: err.message });
+                }
+            });
 
-    return () => {
-      isMounted = false;
-    };
-  }, [showId]);
+        return () => {
+            isMounted = false;
+        };
+    }, [showId]);
 
-  return state;
+    return state;
 }
