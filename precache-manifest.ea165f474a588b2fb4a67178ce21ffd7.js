c5c8811410954ebf4512a20aa29b300fc02700c9
Suryansh Srivastava <suryanshsriv1@gmail.com>
2021-09-12 20:26:16 +0530

    Updates

---
+++ precache-manifest.ea165f474a588b2fb4a67178ce21ffd7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "95ec9a9c22730096a31754b09e223ade",
+    "url": "/box-office-app/index.html"
+  },
+  {
+    "revision": "278fa763e8f63e074f8b",
+    "url": "/box-office-app/static/css/main.93052401.chunk.css"
+  },
+  {
+    "revision": "5ad4ba4a414574b6f22e",
+    "url": "/box-office-app/static/js/2.3a305c1d.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/box-office-app/static/js/2.3a305c1d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "278fa763e8f63e074f8b",
+    "url": "/box-office-app/static/js/main.32124f75.chunk.js"
+  },
+  {
+    "revision": "e84a49faa35fea3d975c",
+    "url": "/box-office-app/static/js/runtime-main.27edc3ee.js"
+  }
+]);
